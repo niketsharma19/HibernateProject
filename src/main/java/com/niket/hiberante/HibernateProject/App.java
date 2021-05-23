7b788d5063bae2e702a623e3f9606fddcea23807
Niket Sharma <niketsharma22@gmail.com>
2021-05-23 13:06:01 +0530

    initial setup for Hibernate project

---
+++ src/main/java/com/niket/hiberante/HibernateProject/App.java
@@ -0,0 +1,13 @@
+package com.niket.hiberante.HibernateProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
